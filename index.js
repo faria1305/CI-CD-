432a77b52ed0d0721af9dea3979b88202fb9e85f
Faria Akter <fariaakter@Farias-Laptop.local>
2025-01-06 21:22:46 +0600

    solve static file upload problem

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+// if (require.main === module) {
+//   app.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}/`);
+//   });
+// }
+
+// module.exports = app;
+
 const express = require('express');
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-if (require.main === module) {
-  app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
-  });
-}
-
-module.exports = app;
+module.exports = app; // Export the app for Vercel
