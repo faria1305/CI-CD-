82258a566694e0ee7e1d852ac8cf1a893b62a9ae
Faria Akter <fariaakter@Farias-Laptop.local>
2025-01-07 10:11:40 +0600

    test

--- test/app.test.js
+++ test/app.test.js
@@ -2,10 +2,10 @@ const request = require('supertest');
 const app = require('../index');
 
 describe('GET /', () => {
-  it('should return Hello World!', async () => {
+  it('should return Hello World2!', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('Hello World!');
+    expect(res.text).toBe('Hello World2!');
   });
 
 //   it('should fail if the response is not Hello World!', async () => {
